3c285309df7aa1a57f3b567a65d7969112f14238
Fred Zimmerman <fredzannarbor@gmail.com>
2024-07-20 10:24:06 -0400

    Remove main.py and add setup.py
    Removed the main.py script as it was only a sample Python script. Added a new setup.py file to package 'Codexes2Gemini' with related details such as version, author, email, license etc. The new file will facilitate the package installation and distribution.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Codexes2Gemini',
+    version='0.1',
+    description='Humans and large-context language models making books richer, more diverse, and more surprising.',
+    url='https://github.com/fredzannarbor/Codexes2Gemini',
+    author='Fred_Zimmerman',
+    author_email='wfz@nimblebooks.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
