805db3bf3bc2666b4d1f400ea06fea077692075c
Fred Zimmerman <fredzannarbor@gmail.com>
2024-08-11 19:04:15 -0400

    Update setup.py to version 0.2.1.0 and improve package data
    Updated the `setup.py` file to reflect version `0.2.1.0` with enhancements in package metadata. This includes correcting the casing in the `entry_points` for `codexes2gemini-ui` and `codexes
    
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='Codexes2Gemini',
-    version='0.2.0.4',
+    version='0.2.1.0',
     python_requires='>=3.11',
     description='Humans and AIs making books richer, more diverse, and more surprising.',
     url='https://github.com/fredzannarbor/Codexes2Gemini',
     entry_points={
         'console_scripts': [
-            'codexes2gemini-ui = Codexes2Gemini.UI.streamlit_ui:main',
-            'codexes2gemini-ui-1455 = Codexes2Gemini.UI.streamlit_ui:main'
+            'codexes2gemini-ui = Codexes2Gemini.ui.streamlit_ui:main',
+            'codexes2gemini-ui-1455 = Codexes2Gemini.ui.streamlit_ui:main'
         ]
     },
     author='Fred_Zimmerman',
@@ -18,7 +18,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'Codexes2Gemini': ['resources/prompts/*.json'],
+        'Codexes2Gemini': ['resources/prompts/*.json', 'resources/images/*.jpg', 'resources/images/*.png',
+                           'resources/images/*.jpeg'],
     },
     install_requires=['streamlit'],
     classifiers=[
@@ -27,6 +28,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
