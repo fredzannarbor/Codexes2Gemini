27d3f5e49592e773915687d0e1d16aa25256de30
Fred Zimmerman <fredzannarbor@gmail.com>
2024-08-16 04:40:37 -0400

    Update version and dependencies in setup.py
    Bumped the version of Codexes2Gemini from 0.2.2.0 to 0.2.2.5 to reflect the new changes. Added 'google-generativeai' and 'google-cloud-texttospeech' to the list of install_requires for enhanced functionality involving Google AI and cloud text-to-speech services. This update ensures smoother integrations and adds new features related to AI capabilities. Updated dependencies are essential for continued development and to leverage Google's advanced AI tools.
    
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Codexes2Gemini',
-    version='0.2.2.0',
+    version='0.2.2.5',
     python_requires='>=3.11',
     description='Humans and AIs making books richer, more diverse, and more surprising.',
     url='https://github.com/fredzannarbor/Codexes2Gemini',
@@ -21,7 +21,8 @@ setup(
         'Codexes2Gemini': ['resources/prompts/*.json', 'resources/images/*.jpg', 'resources/images/*.png',
                            'resources/images/*.jpeg', 'resources/images/*.json'],
     },
-    install_requires=['streamlit', 'pymupdf', 'pypandoc', 'python-docx'],
+    install_requires=['streamlit', 'pymupdf', 'pypandoc', 'python-docx', 'google-generativeai',
+                      'google-cloud-texttospeech'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
