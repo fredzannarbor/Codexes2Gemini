adf2f71a641bcf0fec16495ac77e4e94e6324360
Fred Zimmerman <fredzannarbor@gmail.com>
2024-08-22 21:45:31 -0400

    chore: Bump version to 0.2.2.19 This commit updates the version number in setup.py to 0.2.2.19 to reflect the latest changes in the project.
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Codexes2Gemini',
-    version='0.2.2.16',
+    version='0.2.2.19',
     python_requires='>=3.11',
     description='Humans and AIs making books richer, more diverse, and more surprising.',
     url='https://github.com/fredzannarbor/Codexes2Gemini',
@@ -21,7 +21,7 @@ setup(
         'Codexes2Gemini': ['resources/prompts/*.json', 'resources/images/*.jpg', 'resources/images/*.png',
                            'resources/images/*.jpeg', 'resources/images/*.json'],
     },
-    install_requires=['streamlit', 'pymupdf', 'pypandoc', 'python-docx', 'google-generativeai', 'docx2txt',
+    install_requires=['streamlit', 'pymupdf', 'pypandoc', 'python-docx', 'google-generativeai', 'docx2txt', 'chardet',
                       'google-cloud-texttospeech'],
     classifiers=[
         'Development Status :: 3 - Alpha',
