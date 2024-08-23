ef01729a57bdd862bdc1608202edf48465f88172
Fred Zimmerman <fredzannarbor@gmail.com>
2024-08-22 21:32:52 -0400

    feat: Enhance resource packaging, streamline logging, and add self-identification
    This commit introduces several improvements to Codexes2Gemini:
    - adds chardet to required packages
    - Bumps version to 0.2.2.19 to reflect these changes.
    
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- Codexes2Gemini/__init__.py
+++ Codexes2Gemini/__init__.py
@@ -13,3 +13,12 @@ ensure_directory_exists("output/c2g")
 ensure_directory_exists("logs")
 ensure_directory_exists("userspaces")
 ensure_directory_exists("userspaces/self")
+
+__version__ = "0.2.2.19"
+__announcements__ = """
+- Adds chardet to required packages
+---
+- **Improves packaging of resources to avoid file not found errors**, ensure forward compatibility.
+- Streamlines logging.
+- Adds self-identification.
+"""
