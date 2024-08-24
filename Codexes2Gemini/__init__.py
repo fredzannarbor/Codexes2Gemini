610e9f6fe4c853557b6fdfbc722f9cdd4da4dec0
Fred Zimmerman <fredzannarbor@gmail.com>
2024-08-24 01:37:00 -0400

    bumps to 0.2.3.0 adds JSON support
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- Codexes2Gemini/__init__.py
+++ Codexes2Gemini/__init__.py
@@ -14,11 +14,8 @@ ensure_directory_exists("logs")
 ensure_directory_exists("userspaces")
 ensure_directory_exists("userspaces/self")
 
-__version__ = "0.2.2.19"
+__version__ = "0.2.3.0"
 __announcements__ = """
-- Adds chardet to required packages
----
-- **Improves packaging of resources to avoid file not found errors**, ensure forward compatibility.
-- Streamlines logging.
-- Adds self-identification.
+- PromptPlans are refactored to PromptGroups to make function more clear.
+- Simple JSON objects may now be updated and their text tokenized.
 """
