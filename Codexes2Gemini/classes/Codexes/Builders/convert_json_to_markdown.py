50e8b050f015155017e5b33c800f2a9c6eff994c
Fred Zimmerman <fredzannarbor@gmail.com>
2024-09-19 22:59:33 -0400

    utility script to convert json to markdown
    Signed-off-by: Fred Zimmerman <fredzannarbor@gmail.com>

--- Codexes2Gemini/classes/Codexes/Builders/convert_json_to_markdown.py
+++ Codexes2Gemini/classes/Codexes/Builders/convert_json_to_markdown.py
@@ -1,11 +1,14 @@
 import json
 
-with open('/Users/fred/bin/nimble/stable_c2g/processed_data/33303.json', 'r') as f:
+with open('/Users/fred/bin/nimble/stable_c2g/output/Tonkin.json', 'r') as f:
     data = json.load(f)
 
 markdown_text = ""
 
-for result in data["results"]:
-    markdown_text += result + "\n\n"
+for index, result in enumerate(data[0]):
+    markdown_text += result[index] + "\n\n"
 
 print(markdown_text)
+
+with open('output/results.md', 'w') as f:
+    f.write(markdown_text)
